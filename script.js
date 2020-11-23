e8f0ed26570d2c3ef051d465e2bf7e743dd380c9
taran-duba <72531052+taran-duba@users.noreply.github.com>
2020-11-23 17:56:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var first_name = "Taran ";
+var last_name = "Duba";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var num1 = 9
+var num2 = 7
+var add = num1 + num2;
+console.log(add);
+var subtract = num1 - num2;
+console.log(subtract);
+var multiply = num1 * num2;
+console.log(multiply);
+var divide = num1/num2;
+console.log(divide);
